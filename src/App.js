5d98b2d6bf883e03dc7a829a8e0de36291f918ed
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-03-01 17:01:46 +0100

    useEffect hook example

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // runs once when app starts up, because no deps are specified
+  useEffect(() => {
+    const isLoggedInInfo = localStorage.getItem("isLoggedIn");
+    if (isLoggedInInfo === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
-    // But it's just a dummy/ demo anyways
+    // But it's just a dummy/ demo anyway
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
