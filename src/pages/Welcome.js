806ad205d861170e595e5eaecdd06d3169fa6b3b
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-03-27 17:33:38 +0200

    redirect

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,10 @@
+import {Route} from "react-router-dom";
+
 const Welcome = () => {
-    return <h1>The Welcome page</h1>
+    return <section><h1>The Welcome page</h1>
+    <Route path="/welcome/new-user">
+        <p>Welkom new user</p>
+    </Route></section>
 };
 
 export default Welcome;
