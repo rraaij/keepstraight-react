511694820adf1ce7a56dc1b40659e9ce8da46b93
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-05-04 11:43:08 +0200

    async test

---
+++ src/components/Async.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import Async from "./Async";
+
+describe("Async component", () => {
+  test("renders posts if request succeeds", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{ id: "p1", title: "First Post" }],
+    });
+    render(<Async />);
+
+    const listitems = await screen.findAllByRole("listitem");
+    expect(listitems).not.toHaveLength(0);
+  });
+});
