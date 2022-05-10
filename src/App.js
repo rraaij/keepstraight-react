34dee55b7e33860b964de9d2661a89b7256cc8cf
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-05-10 10:34:48 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-function App() {
-  return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
-  );
-}
+import React from 'react';
+
+import Ingredients from './components/Ingredients/Ingredients';
+
+const App = props => {
+  return <Ingredients />;
+};
 
 export default App;
