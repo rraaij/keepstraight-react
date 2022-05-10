e3e6291c933293e21e3ad4d65380af4851bd44d9
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-05-10 12:37:35 +0200

    useRef

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,19 +3,23 @@ import React, { useCallback, useEffect, useState } from "react";
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
+import ErrorModal from "../UI/ErrorModal";
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const firebaseUrl =
-    "https://keepstraight-backend-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json";
+    "https://keepstraight-backend-default-rtdb.europe-west1.firebasedatabase.app";
 
   const filteredIngredientsHandler = useCallback((filteredIngredients) => {
     setIngredients(filteredIngredients);
   }, []);
 
   const addIngredientHandler = (ingredient) => {
-    fetch(firebaseUrl, {
+    setLoading(true);
+    fetch(`${firebaseUrl}/ingredients.json`, {
       method: "POST",
       body: JSON.stringify(ingredient),
       headers: { "Content-Type": "application/json" },
@@ -29,18 +33,36 @@ const Ingredients = () => {
             ...ingredient,
           },
         ]);
+        setLoading(false);
       });
   };
 
   const removeIngredientHandler = (id) => {
-    setIngredients((prevState) => [
-      ...prevState.filter((ingredient) => ingredient.id !== id),
-    ]);
+    setLoading(true);
+    fetch(`${firebaseUrl}/ingredients/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => {
+        setIngredients((prevState) => [
+          ...prevState.filter((ingredient) => ingredient.id !== id),
+        ]);
+        setLoading(false);
+      })
+      .catch((error) => setError("!!! OHDJEEE!!! :" + error.message));
   };
+
+  const clearError = () => {
+      setError(null);
+      setLoading(false);
+  }
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler} />
+      {error && <ErrorModal onClose={clearError}>{error}</ErrorModal>}
+      <IngredientForm
+        onAddIngredient={addIngredientHandler}
+        loading={isLoading}
+      />
 
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -6,28 +6,35 @@ import "./Search.css";
 const Search = React.memo((props) => {
   const firebaseUrl =
     "https://keepstraight-backend-default-rtdb.europe-west1.firebasedatabase.app/ingredients.json";
-  const [enteredFilter, setEnteredFilter] = useState("");
   const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState("");
+  const inputRef = useRef();
 
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch(firebaseUrl + query)
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(firebaseUrl + query)
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-        onLoadIngredients(loadedIngredients);
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      }
+    }, 500);
+    // return cleanup function
+    return () => clearTimeout(timer);
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -35,6 +42,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={(event) => setEnteredFilter(event.target.value)}
