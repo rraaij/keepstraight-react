79a2b354ff1bd98d3da3c38fde64aee2935d39bd
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-05-11 09:43:00 +0200

    useContext

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 
+import Auth from "./components/Auth";
+import { AuthContext } from "./context/auth-context";
 import Ingredients from "./components/Ingredients/Ingredients";
 
 const App = (props) => {
-  return <Ingredients />;
+  const authContext = useContext(AuthContext);
+
+  let content = <Auth />;
+  if (authContext.isAuth) {
+    content = <Ingredients />;
+  }
+  return content;
 };
 
 export default App;
